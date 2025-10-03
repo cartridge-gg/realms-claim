3735881374fc3612a7d6d03577f53e5a18bf821b
ayushtom <tomar2585@gmail.com>
2025-10-03 17:29:25 +0530

    setup base client and server

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(3000, async () => {
+    console.log("Server ready on port ", 3000);
+});
+
+
